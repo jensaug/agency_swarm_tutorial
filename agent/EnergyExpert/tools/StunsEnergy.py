dc5b4b2a78066aa4404f6e5834488c7fa04d2ee5
Jens X Augustsson <jens@augustson.eu>
2024-11-08 17:10:07 +0100

    Removed erronous tool description

--- agent/EnergyExpert/tools/StunsEnergy.py
+++ agent/EnergyExpert/tools/StunsEnergy.py
@@ -3,8 +3,6 @@ from agency_swarm.tools import BaseTool
 
 class StunsEnergy(BaseTool):
     """
-    This is a website scraping tool. You can use this tool to scrape the content of a website.
-    You must provide the URL of the website you want to scrape.
     This is the StunsEnergy tool. Use this tool to access for data about energy consumption at Tiunda school
     When using StunsEnergy tool you must select the best date and time to find the relevant energy usage.
     The source API for StunsEnergy tool is found here: https://www.dataportal.se/sv/datasets/763_1422/tiunda-skola-uppsala-energimatning-15-minuters-data/apiexplore/1415
